c021f55707e0ace0b85de7babc378cb4e40b92c1
Khrystyna Kindrat <102920051+ChristabelKindrat@users.noreply.github.com>
2023-04-05 19:37:45 +0300

    Update HealthcheckServlet.java
    change HealthecheckServlet into normal :)

--- src/main/java/com/vansisto/servlet/HealthcheckServlet.java
+++ src/main/java/com/vansisto/servlet/HealthcheckServlet.java
@@ -1,9 +1,5 @@
 package com.vansisto.servlet;
 
-import com.vansisto.model.Product;
-import com.vansisto.service.ProductService;
-import com.vansisto.service.impl.ProductServiceImpl;
-import com.vansisto.util.RestUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,40 +10,10 @@ import java.io.IOException;
 
 @WebServlet("/healthcheck")
 public class HealthcheckServlet extends HttpServlet {
-    private ProductService productService = new ProductServiceImpl();
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Product product = RestUtil.getFromJson(req, Product.class);
-        int status = productService.create(product);
-        resp.setStatus(status);
-        resp.getWriter().write("Successfully created!");
-    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("id");
-        String respJson = RestUtil.toJson(productService.getById(Long.parseLong(id)));
-
-        resp.getWriter().write(respJson);
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("id");
-        int status = productService.deleteById(Long.parseLong(id));
-
-        resp.setStatus(status);
-        resp.getWriter().write("Successfully deleted!");
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Product product = RestUtil.getFromJson(req, Product.class);
-        String id = req.getParameter("id");
-        int status = productService.updateById(product, Long.parseLong(id));
-
-        resp.setStatus(status);
-        resp.getWriter().write("Successfully update!");
+      resp.setStatus(200);
+      resp.getWriter().write("Success connect!");
     }
 }
